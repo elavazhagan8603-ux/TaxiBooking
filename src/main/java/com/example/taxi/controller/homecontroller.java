95f13fe5eb505a7589781878669388b9c80a47b2
elavazhagan8603-ux <elavazhagan8603@gmail.com>
2026-06-14 17:35:41 +0530

    Add HomeController to fix 404 error

---
+++ src/main/java/com/example/taxi/controller/homecontroller.java
@@ -0,0 +1,14 @@
+package com.example.taxi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homecontroller {
+
+    @GetMapping("/")
+    public String index() {
+        // This looks for a file named "index.html" in src/main/resources/templates
+        return "index"; 
+    }
+}
